fe8710b5ff979c74be8649662350eee1176b6278
Amen Endriyas <amenendriyas@gmail.com>
2026-05-03 15:03:26 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        MusicItem item = new MusicItem("Test Song", "Test Artist", 3.5);
-        item.displayInfo();
+       
+        MusicItem song = new Song("HOPE", " NF ", 3.5);
+        MusicItem podcast = new Podcast("Wechew GooD", "Yonzima, Mahi & Bereket", 53.0, 12);
+        
+        System.out.println("========== Display Info =======");
+        song.displayInfo();
+        podcast.displayInfo();
+
+        System.out.println("\n=== playing ===");
+
+        ((Playable) song).play();
+        ((Playable) podcast).play();
+
+
+
     }
 }
